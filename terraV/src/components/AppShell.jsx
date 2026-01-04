238e09daf526deafc150ef57a78cfaec13d2d667
ImAsu1304 <moharanaasu009@gmail.com>
2026-01-04 18:09:35 +0530

    Added the AnalysisController logic

--- terraV/src/components/AppShell.jsx
+++ terraV/src/components/AppShell.jsx
@@ -4,10 +4,12 @@ import MapCanvas from "./MapCanvas";
 // import RightDockPlaceholder from "./RightDockPlaceholder";
 import RegionSelector from "./RegionSelector";
 import ClassControlDock from "./ClassControlDock";
+import AnalysisController from "./AnalysisContoller";
 
 const AppShell = () => {
 
     const [region, setRegion] = useState(null);
+    const [analysisRequest, setAnalysisRequest] = useState(null)
 
     return (
         <div className="h-screen w-screen flex flex-col bg-black text-white">
@@ -16,7 +18,16 @@ const AppShell = () => {
             <div className="flex flex-1 overflow-hidden">
                 <MapCanvas region={region}/>
                 {/* <RightDockPlaceholder/> */}
-                {region?.confirmed ? (<ClassControlDock region={region}/>) : (<RegionSelector region={region} setRegion={setRegion} />)}
+                
+                {region?.confirmed ? (<ClassControlDock region={region} onRun={setAnalysisRequest} />) : (<RegionSelector region={region} setRegion={setRegion} />)}
+
+                {analysisRequest && (
+                    <AnalysisController
+                    analysisRequest={analysisRequest}
+                    onComplete={() => setAnalysisRequest(null)}
+                    onCancel={() => setAnalysisRequest(null)}
+                    />
+                )}
             </div>
         </div>
     );
