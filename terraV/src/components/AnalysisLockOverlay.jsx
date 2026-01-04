e02c76fde1e06e2913d0d535c992d41ced74f425
ImAsu1304 <moharanaasu009@gmail.com>
2026-01-04 18:13:17 +0530

    This prevents user actions when analysis takes place and reduces opacity (looks like app is busy)

---
+++ terraV/src/components/AnalysisLockOverlay.jsx
@@ -0,0 +1,7 @@
+const AnalysisLockOverlay = () => {
+    return (
+        <div className="absolute inset-0 bg-black/50 z-10 pointer-events-auto"></div>
+    );
+};
+
+export default AnalysisLockOverlay;
