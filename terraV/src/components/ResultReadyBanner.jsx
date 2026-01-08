bbb98065a078093e1025ae2c23f9c7bebc734784
ImAsu1304 <moharanaasu009@gmail.com>
2026-01-08 16:14:37 +0530

    Shown if analysis is completed.

---
+++ terraV/src/components/ResultReadyBanner.jsx
@@ -0,0 +1,9 @@
+const ResultReadyBanner = () => {
+  return (
+    <div className="absolute top-14 left-1/2 -translate-x-1/2 bg-green-600 text-black text-sm px-4 py-1">
+      Analysis Completed
+    </div>
+  );
+};
+
+export default ResultReadyBanner;
