f1285a00c70cb006997e48c2723042efcadf714c
ImAsu1304 <moharanaasu009@gmail.com>
2026-02-01 01:11:48 +0530

    this is a custom alert for app under development

---
+++ frontend/src/components/UnderDevErr.jsx
@@ -0,0 +1,9 @@
+const UnderDevErr = () => {
+    <div className="fixed inset-0 z-500 flex items-center justify-center pointer-events-none">
+        <div className="w-80 p-4 bg-gray-900 border border-gray-700 rounded-xl shadow-2xl pointer-events-auto">
+            <p>This is under dev</p>
+        </div>
+    </div>
+}
+
+export default UnderDevErr;
