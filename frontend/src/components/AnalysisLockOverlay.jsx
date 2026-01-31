c13e9d0a6ad440aefb692397247a33fb971fe285
ImAsu1304 <moharanaasu009@gmail.com>
2026-02-01 00:45:35 +0530

    It adds a black curtain while analysis to stop interaction visually

---
+++ frontend/src/components/AnalysisLockOverlay.jsx
@@ -0,0 +1,7 @@
+const AnalysisLockOverlay = () => {
+    return (
+        <div className="absolute inset-0 bg-black/50 z-403 pointer-events-auto backdrop-blur-lg"></div>
+    );
+};
+
+export default AnalysisLockOverlay;
